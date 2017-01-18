a0b1627ece88d69b66890320fc46d9e9d96d081d
meugen <meugen.82@gmail.com>
2017-01-18 21:23:38 +0200

    Implemented first scenario

--- app/src/main/java/ua/meugen/android/levelup/homework7/Scenario1Activity.java
+++ app/src/main/java/ua/meugen/android/levelup/homework7/Scenario1Activity.java
@@ -1,6 +1,8 @@
 package ua.meugen.android.levelup.homework7;
 
 import android.animation.TimeInterpolator;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
 import android.transition.ChangeBounds;
 import android.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
@@ -22,6 +24,26 @@ public class Scenario1Activity extends AppCompatActivity implements View.OnClick
 
     private ChangeBounds changeBounds;
 
+    private final TimeInterpolator interpolator = new AccelerateInterpolator(2f);
+    private final long duration = TimeUnit.SECONDS.toMillis(2);
+    private final ValueAnimator.AnimatorUpdateListener textColorUpdateListener = new ValueAnimator.AnimatorUpdateListener() {
+        @Override
+        public void onAnimationUpdate(final ValueAnimator animator) {
+            final int color = (Integer) animator.getAnimatedValue();
+            button.setTextColor(color);
+        }
+    };
+    private final ValueAnimator.AnimatorUpdateListener textSizeUpdateListener = new ValueAnimator.AnimatorUpdateListener() {
+        @Override
+        public void onAnimationUpdate(final ValueAnimator animator) {
+            final float textSize = (Float) animator.getAnimatedValue();
+            button.setTextSize(textSize);
+        }
+    };
+
+    private float smallFontSize;
+    private float bigFontSize;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,8 +54,9 @@ public class Scenario1Activity extends AppCompatActivity implements View.OnClick
         button.setOnClickListener(this);
         root = (ViewGroup) findViewById(R.id.activity_scenario_1);
 
-        final TimeInterpolator interpolator = new AccelerateInterpolator(2f);
-        final long duration = TimeUnit.SECONDS.toMillis(2);
+        smallFontSize = 20;
+        bigFontSize = smallFontSize * 1.2f;
+        button.setTextSize(smallFontSize);
 
         changeBounds = new ChangeBounds();
         changeBounds.addTarget(button);
@@ -52,20 +75,37 @@ public class Scenario1Activity extends AppCompatActivity implements View.OnClick
     private void buttonClicked() {
         TransitionManager.beginDelayedTransition(root, changeBounds);
 
+        ValueAnimator textColorAnimator;
+        ValueAnimator textSizeAnimator;
+
         RelativeLayout.LayoutParams params;
         if (changed) {
             params = new RelativeLayout.LayoutParams(
                     RelativeLayout.LayoutParams.WRAP_CONTENT,
                     RelativeLayout.LayoutParams.WRAP_CONTENT);
             params.addRule(RelativeLayout.CENTER_IN_PARENT);
+            textColorAnimator = ValueAnimator.ofArgb(Color.RED, Color.BLACK);
+            textSizeAnimator = ValueAnimator.ofFloat(bigFontSize, smallFontSize);
         } else {
             params = new RelativeLayout.LayoutParams(
                     Math.round(button.getMeasuredWidth() * 1.5f),
                     button.getMeasuredHeight() * 2);
             params.addRule(RelativeLayout.ALIGN_PARENT_END);
             params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+            button.setTextColor(Color.RED);
+            button.setTextSize(bigFontSize);
+            textColorAnimator = ValueAnimator.ofArgb(Color.BLACK, Color.RED);
+            textSizeAnimator = ValueAnimator.ofFloat(smallFontSize, bigFontSize);
         }
         changed = !changed;
+        textColorAnimator.addUpdateListener(textColorUpdateListener);
+        textColorAnimator.setInterpolator(interpolator);
+        textColorAnimator.setDuration(duration);
+        textSizeAnimator.addUpdateListener(textSizeUpdateListener);
+        textSizeAnimator.setInterpolator(interpolator);
+        textSizeAnimator.setDuration(duration);
         button.setLayoutParams(params);
+        textColorAnimator.start();
+        textSizeAnimator.start();
     }
 }
