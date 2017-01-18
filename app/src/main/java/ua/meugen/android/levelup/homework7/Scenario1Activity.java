51ef9fb067bde34707b25c40ec2769b42810b3ba
emalyshev <emalyshev@wellink.ru>
2017-01-18 10:55:31 +0200

    Intermediate commit

--- app/src/main/java/ua/meugen/android/levelup/homework7/Scenario1Activity.java
+++ app/src/main/java/ua/meugen/android/levelup/homework7/Scenario1Activity.java
@@ -1,19 +1,26 @@
 package ua.meugen.android.levelup.homework7;
 
+import android.animation.TimeInterpolator;
+import android.transition.ChangeBounds;
+import android.transition.TransitionManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.transition.TransitionManager;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 
+import java.util.concurrent.TimeUnit;
+
 public class Scenario1Activity extends AppCompatActivity implements View.OnClickListener {
 
-    private boolean fullActivity = false;
+    private boolean changed = false;
 
     private ViewGroup root;
     private Button button;
+
+    private ChangeBounds changeBounds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +31,14 @@ public class Scenario1Activity extends AppCompatActivity implements View.OnClick
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(this);
         root = (ViewGroup) findViewById(R.id.activity_scenario_1);
+
+        final TimeInterpolator interpolator = new AccelerateInterpolator(2f);
+        final long duration = TimeUnit.SECONDS.toMillis(2);
+
+        changeBounds = new ChangeBounds();
+        changeBounds.addTarget(button);
+        changeBounds.setInterpolator(interpolator);
+        changeBounds.setDuration(duration);
     }
 
     @Override
@@ -35,22 +50,22 @@ public class Scenario1Activity extends AppCompatActivity implements View.OnClick
     }
 
     private void buttonClicked() {
-        TransitionManager.beginDelayedTransition(root);
+        TransitionManager.beginDelayedTransition(root, changeBounds);
 
         RelativeLayout.LayoutParams params;
-        if (fullActivity) {
+        if (changed) {
             params = new RelativeLayout.LayoutParams(
                     RelativeLayout.LayoutParams.WRAP_CONTENT,
                     RelativeLayout.LayoutParams.WRAP_CONTENT);
             params.addRule(RelativeLayout.CENTER_IN_PARENT);
         } else {
             params = new RelativeLayout.LayoutParams(
-                    RelativeLayout.LayoutParams.MATCH_PARENT,
-                    RelativeLayout.LayoutParams.MATCH_PARENT);
-            params.removeRule(RelativeLayout.CENTER_IN_PARENT);
+                    Math.round(button.getMeasuredWidth() * 1.5f),
+                    button.getMeasuredHeight() * 2);
+            params.addRule(RelativeLayout.ALIGN_PARENT_END);
+            params.addRule(RelativeLayout.ALIGN_PARENT_TOP);
         }
-        fullActivity = !fullActivity;
+        changed = !changed;
         button.setLayoutParams(params);
-        button.requestLayout();
     }
 }
