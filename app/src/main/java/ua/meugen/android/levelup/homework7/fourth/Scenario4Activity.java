57681bdf34898371f6e94961e693b7464850ee76
meugen <meugen.82@gmail.com>
2017-01-22 13:13:02 +0200

    Implemented scenario 4

--- app/src/main/java/ua/meugen/android/levelup/homework7/fourth/Scenario4Activity.java
+++ app/src/main/java/ua/meugen/android/levelup/homework7/fourth/Scenario4Activity.java
@@ -1,11 +1,17 @@
 package ua.meugen.android.levelup.homework7.fourth;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import ua.meugen.android.levelup.homework7.R;
 
-public class Scenario4Activity extends AppCompatActivity {
+public class Scenario4Activity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,15 @@ public class Scenario4Activity extends AppCompatActivity {
         setContentView(R.layout.activity_scenario_4);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        image = (ImageView) findViewById(R.id.image);
+        image.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(final View v) {
+        final Drawable drawable = image.getDrawable();
+        if (drawable instanceof Animatable) {
+            ((Animatable) drawable).start();
+        }
     }
 }
